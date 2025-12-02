f040a6ba68e9732d5b93e95bf0c04c4d0390c902
21070573-pixel <21070573@vnu.edu.vn>
2025-12-02 16:52:43 +0700

    Fix session check for username in home.php

--- midterm/home.php
+++ midterm/home.php
@@ -1,7 +1,7 @@
 <?php
 include("connection.php");
 session_start();
-if (!isset($_SESSION['username'])) {
+if (!isset($_SESSION['username'])) {   
     header("Location: login.php");
     exit();
 }
